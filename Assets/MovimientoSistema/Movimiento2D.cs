75c16cac644b5c1a19e38e0591e284b70b474c78
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-09-16 19:57:54 -0500

    feat(movement): optimized the movement and added animations

--- Assets/MovimientoSistema/Movimiento2D.cs
+++ Assets/MovimientoSistema/Movimiento2D.cs
@@ -2,26 +2,28 @@ using UnityEngine;
 
 public class Movimiento2D : MonoBehaviour
 {
-    public Controles controles;
+    [SerializeField] private Controles controles;
+    [SerializeField] private Rigidbody2D rb2D;
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float fuerzaSalto;
+    [SerializeField] private LayerMask queEsSuelo;
+    [SerializeField] private Transform controladorSuelo;
+    [SerializeField] private Vector3 dimensionesCaja;
+    private bool enSuelo;
+    private Animator animator;
+    private Vector2 direccion;
+    private bool mirandoDerecha = true;
 
-    public Vector2 direccion;
-
-    public Rigidbody2D rb2D;
-
-    public float velocidadMovimiento;
-
-    public bool mirandoDerecha = true;
-
-    public float fuerzaSalto;
-
-    public LayerMask queEsSuelo;
-    public Transform controladorSuelo;
-    public Vector3 dimensionesCaja;
-    public bool enSuelo;
+    private bool haSaltado = false;
 
     private void Awake()
     {
-        controles = new();
+        controles ??= new Controles();
+    }
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -39,26 +41,34 @@ public class Movimiento2D : MonoBehaviour
     private void Update()
     {
         direccion = controles.Movimiento.Mover.ReadValue<Vector2>();
-        AjustarRotacion(direccion.x); //Cambia la orientacion de sprites
+        AjustarRotacion(direccion.x);
 
-        enSuelo = Physics2D.OverlapBox(controladorSuelo.position, dimensionesCaja, 0f, queEsSuelo);
+        // Parámetros para el Animator
+        animator.SetFloat("Velocidad", Mathf.Abs(direccion.x));
+        animator.SetBool("EnSuelo", enSuelo);
     }
 
     private void FixedUpdate()
     {
         rb2D.linearVelocity = new Vector2(direccion.x * velocidadMovimiento, rb2D.linearVelocityY);
+        enSuelo = Physics2D.OverlapBox(controladorSuelo.position, dimensionesCaja, 0f, queEsSuelo);
+
+        if (!haSaltado && enSuelo)
+        {
+            animator.SetBool("Salto", false);
+            haSaltado = false;
+        }
+        else if (!haSaltado && !enSuelo)
+        {
+            haSaltado = false;
+        }
+        else haSaltado = enSuelo;
     }
 
     private void AjustarRotacion(float direccionX)
     {
-        if (direccionX > 0 && !mirandoDerecha)
-        {
+        if ((direccionX > 0 && !mirandoDerecha) || (direccionX < 0 && mirandoDerecha))
             Girar();
-        }
-        else if (direccionX < 0 && mirandoDerecha)
-        {
-            Girar();
-        }
     }
 
     private void Girar()
@@ -73,15 +83,15 @@ public class Movimiento2D : MonoBehaviour
     {
         if (enSuelo)
         {
-            rb2D.AddForce(new Vector2(0,fuerzaSalto), ForceMode2D.Impulse);
+            rb2D.linearVelocity = new Vector2(rb2D.linearVelocityX, fuerzaSalto);
+            animator.SetBool("Salto", true);
+            haSaltado = true;
         }
-        
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube(controladorSuelo.position, dimensionesCaja);    
+        Gizmos.DrawWireCube(controladorSuelo.position, dimensionesCaja);
     }
-
 }
