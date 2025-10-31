3e49369b20db1b4efc7f1f357cc58f3c5b094d2b
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-10-31 17:00:48 -0500

    feat: added temporary button select

--- Assets/Game/Scripts/MainMenu/MainMenu.cs
+++ Assets/Game/Scripts/MainMenu/MainMenu.cs
@@ -4,7 +4,9 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private Button startButton;
+    //[SerializeField] private Button startButton;
+    [SerializeField] private Button level1Button; //temporary
+    [SerializeField] private Button level2Button; //temporary
     public void Awake()
     {
         AudioManager.Instance.ChangeBackgroundMusic(AudioConstants.MENU);
@@ -12,12 +14,25 @@ public class MainMenu : MonoBehaviour
 
     private void Start()
     {
-        startButton.onClick.AddListener(StartGame);
+        //startButton.onClick.AddListener(StartGame);
+        level1Button.onClick.AddListener(GoToLevel1);
+        level2Button.onClick.AddListener(GoToLevel2);
     }
 
     public void StartGame()
     {
         AudioManager.Instance.StopBackgroundMusic();
         SceneManager.LoadScene("PruebaPowerups");
+    }
+    // Temporary
+    public void GoToLevel1()
+    {
+        AudioManager.Instance.StopBackgroundMusic();
+        SceneManager.LoadScene("1-1");
+    }
+    public void GoToLevel2()
+    {
+        AudioManager.Instance.StopBackgroundMusic();
+        SceneManager.LoadScene("1-2");
     }
 }   
