50a01bfb62f0a7669a9e4604fa5a1b3d7a2d563c
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-10-31 01:12:23 -0500

    feat: added moving platform

---
+++ Assets/Game/Scripts/Environment/MovingPlatform.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    [SerializeField] private Transform pointA;
+    [SerializeField] private Transform pointB;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float waitTime = 1f;
+    private bool isMoving = true;
+    private static WaitForSeconds _waitForSeconds;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        targetPosition = pointB.position;
+        _waitForSeconds = new WaitForSeconds(waitTime);
+    }
+
+    void Update()
+    {
+        MovePlatform();
+    }
+
+    void MovePlatform()
+    {
+        if (!isMoving) return;
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
+        {
+            StartCoroutine(WaitAtPoint());
+            targetPosition = (targetPosition == pointA.position)
+                ? pointB.position
+                : pointA.position;
+        }
+    }
+
+    IEnumerator WaitAtPoint()
+    {
+        isMoving = false;
+        animator.SetBool("isMoving", false);
+        yield return _waitForSeconds;
+        animator.SetBool("isMoving", true);
+        isMoving = true;
+    }
+}
