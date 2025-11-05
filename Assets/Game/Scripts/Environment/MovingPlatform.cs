d20008d7aabac5ba83c24af332091d4eee3960f6
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-11-05 13:50:06 -0500

    feat: refined both levels

--- Assets/Game/Scripts/Environment/MovingPlatform.cs
+++ Assets/Game/Scripts/Environment/MovingPlatform.cs
@@ -27,6 +27,7 @@ public class MovingPlatform : MonoBehaviour
     void MovePlatform()
     {
         if (!isMoving) return;
+        animator.SetBool("isMoving", true);
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
 
         if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
